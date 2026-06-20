06151e16840d799a95c242d919ade05c680d92fc
anis balkhi <anishbalkhi1@gmail.com>
2026-06-20 18:42:11 +0530

    fix: make server port dynamic for deployment environment

--- api/index.js
+++ api/index.js
@@ -19,8 +19,9 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
 
                //api routers
